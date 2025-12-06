34b0c7cc721f9deed006bdef9320e2b76b8c73f4
Ansh Hatwal <103756983+Ansh911@users.noreply.github.com>
2025-12-06 16:22:54 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,84 +1,25 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-class Cell
-{
-    float radius;
-    float offset;
 
-    public:Cell(float r, float o)
-    {
-        radius = r;
-        offset = o;
-    }
-    float getRadius()
-    {
-        return radius;
-    }
-    float getOffest()
-    {
-        return offset;
-    }
-};
-int runSimulation(double y, double offset)
+float best_case(float q, float gama)
 {
-    float radius = 7;
-    Cell cells[] = {
-        Cell(radius, offset),
-        Cell(radius, offset),
-        Cell(radius, 0),
-        Cell(radius, 0),
-        Cell(radius, -offset),
-        Cell(radius, -offset),
-    };
-    float x = 0;
-    for (int i = 0; i < 6; i++)
-    {
-        Cell cell = cells[i];
-        int n = 7;
-        float d = sqrt(3 * n) * cell.getRadius();
-        float q = (d / cell.getRadius()) - cell.getOffest();
-        x = x + pow(q, -y);
-    }
-    float result = 1 / x;
-    cout << "When y = " << y << " & Mobile station is @ " << offset << " Offset from Center "<<endl; cout<<" Co -channel interference is "<<result<<"\n\n "; 
-    return 0;
+    return 1.0f / (6.0f * pow(q, -gama));
+}
+float worst_case(float q, float gama)
+{
+    return 1.0f / (2.0f * pow(q - 1, -gama) + 2.0f * pow(q, -gama) + 2.0f * pow(q + 1, -gama));
 }
 int main()
 {
-    cout << "Running simulation for co-channel Interference" << endl;
-    int option = -1;
-    while (option != 0)
-    {
-        cout << "Where is your mobile station located?" << endl;
-        cout << " 1. At the center (Best Case) "<<endl; cout<<" 2. At R/4 "<<endl; cout<<" 3. At R / 2 "<<endl; cout<<" 4. At 3R / 4 "<<endl; cout<<" 5. At R(Worst Case) "<<endl; cout<<" 0. Exit\n ";
-        cin >>option;
-        if (option == 0)
-        {
-            return 0;
-        }
-        double offset = 0.0;
-        switch (option)
-        {
-        case 1:
-            offset = 0.0;
-            break;
-        case 2:
-            offset = 0.25;
-            break;
-        case 3:
-            offset = 0.5;
-            break;
-        case 4:
-            offset = 0.75;
-            break;
-        case 5:
-            offset = 1.0;
-            break;
-        }
-        int y = 2;
-        cout << "Enter value of gamma" << endl;
-        cin >> y;
-        runSimulation(y, offset);
-    }
+    cout << "Name-  M.Tech CSE Roll no. " << endl;
+    float gama;
+    float q = sqrt(3 * 7);
+    cout << "Enter the value of gama: ";
+    cin >> gama;
+    float res1 = best_case(q, gama);
+    float res2 = worst_case(q, gama);
+    cout << "Best case result: " << res1
+         << " Worst case result: " << res2 << endl;
+    return 0;
 }
